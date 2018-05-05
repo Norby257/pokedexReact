9b995a6d05795e1e598664f929f54905ee822009
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-05 16:16:18 -0500

    initial styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,17 @@ import React, {Component} from 'react';
 import './styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
-      hellohello
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
