f1d75ddb4c5224e438015bd363d9a5b74ab3e667
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-06 17:49:57 -0500

    using array.map to render images complete

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PokeCell from './PokeCell';
+import {pokeClasses} from '../pokeClasses';
 import './styles/PokeList.css';
 
 
 const PokeList = () => {
+    const cells = pokeClasses.map(pokeClass => {
+        return (
+            <PokeCell 
+            key={pokeClass.id}
+            pokeClass={pokeClass}
+            />
+        );
+    });
 return (
     <section className="poke-list">
-        <PokeCell />
-        <PokeCell />
-        <PokeCell />
+    {cells}
     </section>
 )
 }
